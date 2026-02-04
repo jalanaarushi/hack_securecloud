b12b65396283d9d715a807decf436a15b7ee2700
jalanaarushi <jalanaarushi@gmail.com>
2026-02-04 11:00:04 +0530

    Reset repository for personal project

--- backend/config/awsConfig.js
+++
@@ -1,7 +0,0 @@
-const { DynamoDBClient, PutItemCommand, GetItemCommand } = require('@aws-sdk/client-dynamodb');
-const { KMSClient, EncryptCommand, DecryptCommand } = require('@aws-sdk/client-kms');
-
-const dynamoDbClient = new DynamoDBClient({ region: process.env.AWS_REGION });
-const kmsClient = new KMSClient({ region: process.env.AWS_REGION });
-
-module.exports = { dynamoDbClient, kmsClient, PutItemCommand, GetItemCommand, EncryptCommand, DecryptCommand };
--- backend/routes/dataRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dataController = require('../controllers/dataController');
-
-router.post('/', dataController.addData);
-router.get('/:id', dataController.getData);
-
-module.exports = router;
